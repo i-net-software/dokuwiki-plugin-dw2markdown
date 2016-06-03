eafc14a9df3554ddc0f77e2dd966c8f5fe7ece1d
Gerry Weißbach <gerry.w@gammaproduction.de>
2016-06-03 07:06:48 +0200

    In case of not having the submodule: do not set up the renderer.

--- renderer.php
+++ renderer.php
@@ -10,24 +10,29 @@
 if (!defined('DOKU_INC')) die();
 
 require_once DOKU_INC.'inc/parser/renderer.php';
-require_once DOKU_PLUGIN.'dw2markdown/lib/converter/scripts/DokuWikiToMarkdown.php';
 
-class renderer_plugin_dw2markdown extends Doku_Renderer {
 
-    /**
-     * The format this renderer produces
-     */
-    public function getFormat(){
-        return 'dw2markdown';
+if ( file_exists( DOKU_PLUGIN.'dw2markdown/lib/converter/scripts/DokuWikiToMarkdown.php' ) ) {
+    
+    require_once DOKU_PLUGIN.'dw2markdown/lib/converter/scripts/DokuWikiToMarkdown.php';
+    
+    class renderer_plugin_dw2markdown extends Doku_Renderer {
+    
+        /**
+         * The format this renderer produces
+         */
+        public function getFormat(){
+            return 'dw2markdown';
+        }
+    
+        // FIXME implement all methods of Doku_Renderer here
+        public function document_end(){
+            
+            $document = rawWiki( getID() );
+            
+            $converter = new DokuWikiToMarkdown();
+            $this->doc = $converter->convert( $document );
+        }
     }
-
-    // FIXME implement all methods of Doku_Renderer here
-    public function document_end(){
-        
-        $document = rawWiki( getID() );
-        
-        $converter = new DokuWikiToMarkdown();
-        $this->doc = $converter->convert( $document );
-    }
+    
 }
-
